ae67828e734129c5ef4da7421e55f5ed0dbe0b0d
Luis Eduardo Zaldivar Miranda <lalozaldivarbarza@gmail.com>
2024-05-13 17:55:23 -0600

    ya

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import { ChakraProvider, extendTheme } from "@chakra-ui/react";
-import Home from "../src/componets/home";
-import AboutMe from "../src/componets/AboutMe";
-import Projects from "../src/componets/proyectos";
-import SkillsComponent from "../src/componets/habilidades";
-import SocialLinksComponent from "../src/componets/contacto";
-import './i18n'; 
+import Home from "./componets/home";
+import AboutMe from "./componets/AboutMe";
+import Projects from "./componets/proyectos";
+import SkillsComponent from "./componets/habilidades";
+import SocialLinksComponent from "./componets/contacto";
+import './i18n';
 
 const config = {
   initialColorMode: "light",
@@ -18,23 +18,21 @@ const customTheme = extendTheme({ config });
 function App() {
   return (
     <ChakraProvider theme={customTheme}>
-      <Router>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/about" element={<AboutMe />} />
-          <Route
-            path="/projects"
-            element={
-              <Projects
-                imageUrl="ruta/a/la/imagen.jpg"
-                title="Titulo del Proyecto"
-              />
-            }
-          />
-          <Route path="/skills" element={<SkillsComponent />} />
-          <Route path="/contact" element={<SocialLinksComponent />} /> {/* Corregido */}
-        </Routes>
-      </Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<AboutMe />} />
+        <Route
+          path="/projects"
+          element={
+            <Projects
+              imageUrl="ruta/a/la/imagen.jpg"
+              title="Titulo del Proyecto"
+            />
+          }
+        />
+        <Route path="/skills" element={<SkillsComponent />} />
+        <Route path="/contact" element={<SocialLinksComponent />} />
+      </Routes>
     </ChakraProvider>
   );
 }
